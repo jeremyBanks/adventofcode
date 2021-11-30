9e1fb15cdeac677efb1653c3850a6fe4f818da2a
Jeremy <_@jeremy.ca>
2021-11-30 15:23:44 +0000

    r3/812b

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,16 @@ pub trait Solution {
     const DAY: u32;
 
     fn solve() -> (Self::PartOne, Self::PartTwo) {
-        let session_key = std::env::var("CURL_AOC_SESSION").unwrap();
-        let input_url = format!(
-            "https://adventofcode.com/{}/day/{}/input",
-            Self::YEAR,
-            Self::DAY
-        );
         let input_path = format!("./inputs/year{:04}-day{:02}.txt", Self::YEAR, Self::DAY);
 
         let input = std::fs::read_to_string(&input_path).unwrap_or_else(|_| {
-            let input = reqwest::blocking::Client::new().get(input_url)
+            let session_key = std::env::var("CURL_AOC_SESSION").unwrap();
+            let input_url = format!(
+                "https://adventofcode.com/{}/day/{}/input",
+                Self::YEAR,
+                Self::DAY
+            );
+            let mut input = reqwest::blocking::Client::new().get(input_url)
                 .header(reqwest::header::COOKIE, format!("session={}", session_key))
                 .send()
                 .unwrap()
@@ -28,13 +28,15 @@ pub trait Solution {
                 .unwrap()
                 .text()
                 .unwrap();
+            input.push('\n');
 
             std::fs::write(&input_path, &input).unwrap();
 
             input
         });
+        let input = input.trim();
 
-        (Self::part_one(&input), Self::part_two(&input))
+        (Self::part_one(input), Self::part_two(input))
     }
 
     type PartOne: Default = ();
