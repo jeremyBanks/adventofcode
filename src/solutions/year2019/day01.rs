5b8ff6418d2bf61da1ca9fb6cceee9bb41f0448d
Jeremy <_@jeremy.ca>
2021-12-01 14:26:08 +0000

    r20/2583

--- src/solutions/year2019/day01.rs
+++ src/solutions/year2019/day01.rs
@@ -8,15 +8,28 @@ impl crate::Solution for Solution {
     const DAY: u32 = 1;
 
     type PartOne = u32;
-    fn part_one(input: &str) -> u32 {
-        let mut required_fuel = 0;
+    type PartTwo = u32;
+    fn solve(input: &str) -> (u32, u32) {
+        let mut total_fuel_one = 0;
+        let mut total_fuel_two = 0;
 
         let masses = split_and_parse::<u32>(input, "\n");
         for mass in masses {
-            let divided = (mass / 3).checked_sub(2).unwrap();
-            required_fuel += divided;
+            let mut mass = mass;
+            for i in 0.. {
+                let fuel = (mass / 3).checked_sub(2).unwrap_or_default();
+                if fuel > 0 {
+                    if i == 0 {
+                        total_fuel_one += fuel;
+                    }
+                    total_fuel_two += fuel;
+                    mass = fuel;
+                } else {
+                    break;
+                }
+            }
         }
 
-        required_fuel
+        (total_fuel_one, total_fuel_two)
     }
 }
