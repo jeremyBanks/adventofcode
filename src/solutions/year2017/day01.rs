fc0dc41338cdc20486b3012b37be185c1aaf271e
Jeremy <_@jeremy.ca>
2021-12-01 14:17:36 +0000

    r18/063a

--- src/solutions/year2017/day01.rs
+++ src/solutions/year2017/day01.rs
@@ -21,4 +21,22 @@ impl crate::Solution for Solution {
         }
         sum
     }
+
+    type PartTwo = u32;
+    fn part_two(input: &str) -> u32 {
+        let digits = input
+            .chars()
+            .map(|n| n.to_string().parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        let offset = digits.len() / 2;
+
+        let mut sum = 0;
+        for (i, digit) in digits.iter().enumerate() {
+            let next = digits[(i + offset) % input.len()];
+            if *digit == next {
+                sum += digit;
+            }
+        }
+        sum
+    }
 }
