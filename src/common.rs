b848d81e66bf7aeddff2b4b8126662c1f2c235bc
Jeremy Banks <_@jeremy.ca>
2021-12-28 14:52:09 -0500

    yay

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use crate::prelude::*;
 
 pub struct Solution {
@@ -17,6 +19,14 @@ pub trait DebugExt: Debug {
 }
 
 impl<T: Debug> DebugExt for T {}
+
+pub fn int(s: &str) -> i64 {
+    s.parse().unwrap()
+}
+
+pub fn float(s: &str) -> f64 {
+    s.parse().unwrap()
+}
 
 pub trait StrExt: AsRef<str> {
     fn parse_into<T: FromStr>(&self) -> T {
@@ -67,6 +77,37 @@ pub trait StrExt: AsRef<str> {
             Some(index) => (&this[..index], &this[index + 1..]),
             None => (this, ""),
         }
+    }
+
+    fn usize(&self) -> usize {
+        self.as_ref()
+            .parse()
+            .unwrap_or_else(|_| panic!("could not parse as usize"))
+    }
+    fn i64(&self) -> i64 {
+        self.as_ref()
+            .parse()
+            .unwrap_or_else(|_| panic!("could not parse as i64"))
+    }
+    fn f64(&self) -> f64 {
+        self.as_ref()
+            .parse()
+            .unwrap_or_else(|_| panic!("could not parse as f64"))
+    }
+    fn u64(&self) -> u64 {
+        self.as_ref()
+            .parse()
+            .unwrap_or_else(|_| panic!("could not parse as u64"))
+    }
+    fn u8(&self) -> u8 {
+        self.as_ref()
+            .parse()
+            .unwrap_or_else(|_| panic!("could not parse as u8"))
+    }
+    fn isize(&self) -> isize {
+        self.as_ref()
+            .parse()
+            .unwrap_or_else(|_| panic!("could not parse as isize"))
     }
 }
 
@@ -126,4 +167,72 @@ pub fn is_digit(c: &char) -> bool {
 
 pub fn is_not_digit(c: &char) -> bool {
     !('0'..='9').contains(c)
+}
+
+pub const LOWERCASE_ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+
+pub fn rot13(s: impl AsRef<str>) -> String {
+    rot(s, LOWERCASE_ALPHABET, 13)
+}
+
+pub fn rot_lowercase(s: impl AsRef<str>, distance: impl Int) -> String {
+    rot(s, LOWERCASE_ALPHABET, distance)
+}
+
+pub fn rot(s: impl AsRef<str>, alphabet: &str, distance: impl Int) -> String {
+    let mut s = s.as_ref().chars().collect_vec();
+
+    let distance = distance.i64();
+
+    let alphabet_size = alphabet.len().i64();
+
+    for c in s.iter_mut() {
+        let index = alphabet.find(*c);
+        match index {
+            Some(index) => {
+                let rotated = ((index.i64() + distance) % alphabet_size).usize();
+                *c = alphabet.chars().nth(rotated.usize()).unwrap();
+            },
+            None =>
+                if *c == '-' {
+                    *c = ' ';
+                } else {
+                    dbg!(&c, &index);
+                    panic!("should we be ignoring this?");
+                },
+        }
+    }
+
+    s.iter().join("")
+}
+
+pub trait Int: Sized {
+    fn usize(self) -> usize;
+    fn i64(self) -> i64;
+    fn u64(self) -> u64;
+    fn u8(self) -> u8;
+    fn isize(self) -> isize;
+}
+
+impl<T: TryInto<usize> + TryInto<i64> + TryInto<u64> + TryInto<u8> + TryInto<isize>> Int for T {
+    fn usize(self) -> usize {
+        self.try_into()
+            .unwrap_or_else(|_| panic!("could not convert to usize"))
+    }
+    fn i64(self) -> i64 {
+        self.try_into()
+            .unwrap_or_else(|_| panic!("could not convert to i64"))
+    }
+    fn u64(self) -> u64 {
+        self.try_into()
+            .unwrap_or_else(|_| panic!("could not convert to u64"))
+    }
+    fn u8(self) -> u8 {
+        self.try_into()
+            .unwrap_or_else(|_| panic!("could not convert to u8"))
+    }
+    fn isize(self) -> isize {
+        self.try_into()
+            .unwrap_or_else(|_| panic!("could not convert to isize"))
+    }
 }
--- src/solutions/year2016/day04.rs
+++ src/solutions/year2016/day04.rs
@@ -5,21 +5,24 @@ pub fn solution() -> Solution {
         year: 2016,
         day: 4,
         code: |input| {
-            let mut real_sector_ids_sum: i64 = 0;
+            let mut real_sector_ids_sum = 0;
+            let mut north_pole_sector_id = 0;
 
             for line in input.lines() {
                 // partition the line at last `-` to get name
                 let (name, rest) = line.rpartition("-");
+
                 // partition the rest at first `[` to get the sector ID
                 let (sector_id, rest) = rest.partition("[");
-                let sector_id = sector_id.parse_into::<i64>();
+                let sector_id = int(sector_id);
+
                 // remove the last character (`]`) from the rest to get the checksum
                 let checksum = &rest[..rest.len() - 1];
 
                 let correct_checksum = {
                     let mut letter_counts = HashMap::<char, i32>::new();
                     for character in name.chars() {
-                        if character >= 'a' && character <= 'z' {
+                        if ('a'..='z').contains(&character) {
                             *letter_counts.entry(character).or_default() += 1;
                         } else {
                             assert!(character == '-', "unexpected character");
@@ -38,10 +41,18 @@ pub fn solution() -> Solution {
 
                 if checksum == correct_checksum {
                     real_sector_ids_sum += sector_id;
+                    let decoded = rot_lowercase(name, sector_id);
+                    if decoded.contains("northpole") {
+                        north_pole_sector_id = sector_id;
+                        dbg!(decoded, sector_id);
+                    }
                 }
             }
 
-            (real_sector_ids_sum.to_string(), String::new())
+            (
+                real_sector_ids_sum.to_string(),
+                north_pole_sector_id.to_string(),
+            )
         },
     }
 }
