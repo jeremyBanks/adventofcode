5a01c16bc9f9bca12eded479e9b5298142c31678
Jeremy Banks <_@jeremy.ca>
2021-12-27 20:08:10 -0500

    m

---
+++ src/solutions/year2016/day04.rs
@@ -0,0 +1,11 @@
+use crate::prelude::*;
+
+pub fn solution() -> Solution {
+    Solution {
+        year: 2016,
+        day: 4,
+        code: |input| {
+            todo!()
+        },
+    }
+}
--- src/solutions/year2016/mod.rs
+++ src/solutions/year2016/mod.rs
@@ -2,8 +2,9 @@ use crate::prelude::*;
 
 pub mod day01;
 pub mod day02;
+pub mod day04;
 pub mod day21;
 
 pub fn solutions() -> Vec<Solution> {
-    vec![day01::solution(), day02::solution(), day21::solution()]
+    vec![day01::solution(), day02::solution(), day04::solution(), day21::solution()]
 }
