1b70030fe3056aec2c521da0212b39009b91d4e8
Jeremy Banks <_@jeremy.ca>
2021-12-27 20:16:46 -0500

    m

--- src/solutions/year2016/day04.rs
+++ src/solutions/year2016/day04.rs
@@ -5,7 +5,18 @@ pub fn solution() -> Solution {
         year: 2016,
         day: 4,
         code: |input| {
-            todo!()
+            let lines = input.lines_vec();
+
+            let mut real_sector_ids_sum: u32 = 0;
+
+            for line in lines {
+                let line = line.as_bytes();
+                let last_dash_index = line.iter().rposition(|b| *b == b'-').unwrap();
+                let name = &line[..last_dash_index];
+                println!(name);
+            }
+
+            (String::new(), String::new())
         },
     }
 }
